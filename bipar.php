c11387e11c404b4cdfc18b354a538f5a4eb71eb9
Ramon Santos <ramon.santos@viaexpressa.local>
2021-05-18 14:47:22 -0300

    salvando

---
+++ bipar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+    <div class="contain">
+      <div class="arquivo">
+        <h1>Adicione um arquivo para upload</h1>
+        <form class="" enctype="multipart/form-data" action="processaImportacao.php" method="post">
+          <input type="text" name="file" value="">
+          <input type="submit" name="" value="Enviar arquivo">
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
--- processaImportacao.php
+++ processaImportacao.php
@@ -1,11 +1,44 @@
 <?php
 include_once("conexao.php");
 $file = $_FILES['file'];
-$dir = "./arquivos";
+$dir = "./arquivos/";
 try {
-  move_uploaded_file($file['tmp_name'], $dir);
+  move_uploaded_file($file['tmp_name'], $dir.$file['name']);
 } catch (\Exception $e) {
   echo "erro!";
 }
+$arquivo = new DomDocument();
+$arquivo->load($dir.$file['name']);
+$linhas = $arquivo->getElementsByTagName("Row");
+//var_dump($linhas);
+foreach ($linhas as $linha) {
+  if (is_numeric($linha->getElementsByTagName("Data")->item(0)->nodeValue)) {
+    $filial = $linha->getElementsByTagName("Data")->item(0)->nodeValue;
+    echo "Filial: $filial <br>";
+    $tipo = $linha->getElementsByTagName("Data")->item(1)->nodeValue;
+    echo "Tipo: $tipo <br>";
+    $regional = $linha->getElementsByTagName("Data")->item(2)->nodeValue;
+    echo "Regional: $regional <br>";
+    $divisao = $linha->getElementsByTagName("Data")->item(3)->nodeValue;
+    echo "Divisao: $divisao <br>";
+    $cidade = $linha->getElementsByTagName("Data")->item(4)->nodeValue;
+    echo "Ciadade: $cidade <br>";
+    $estado = $linha->getElementsByTagName("Data")->item(5)->nodeValue;
+    echo "Estado: $estado <br>";
+    $regiao = $linha->getElementsByTagName("Data")->item(6)->nodeValue;
+    echo "Regiao: $regiao <br>";
+    $endereco = $linha->getElementsByTagName("Data")->item(7)->nodeValue;
+    echo "Endereco: $endereco <br>";
+    $gerente = $linha->getElementsByTagName("Data")->item(8)->nodeValue;
+    echo "Gerente: $gerente <br>";
+    $nome_cd = $linha->getElementsByTagName("Data")->item(9)->nodeValue;
+    echo "Nome CD: $nome_cd <br>";
+    $sql = "INSERT INTO tbmDados(FILIAL, TIPO, REGIONAL, DIVISAO, CIDADE, ESTADO, REGIAO, ENDERECO, GERENTE, NOMECD) VALUES('".addslashes($filial)."', '".addslashes($tipo)."', '".addslashes($regional)."', '".addslashes($divisao)."', '".addslashes($cidade)."', '".addslashes($estado)."','".addslashes($regiao)."', '".addslashes($endereco)."', '".addslashes($gerente)."', '".addslashes($nome_cd)."')";
+    $sql = $conn->query($sql) or die($conn->error);
+    echo "<hr>";
+  }else{
+    echo "oi";
+  }
 
+}
  ?>
